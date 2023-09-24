cb16594db261fa868baf885da1d080a2cf1896e1
Axes Musty <>
2023-09-24 14:03:33 +0200

    CSS updates

--- src/App.js
+++ src/App.js
@@ -9,39 +9,42 @@ const App = () => {
     if (false) {test()}
   return(
     <AppProvider>
-       <div className='container'>
-            <div className='rowCommandLine'>
-                <div className='resultbox'>
-                    <CommandLine />
+        <div className='container'>
+            <div className='calculator'>
+                <div className='rowCommandLine'>
+                    <div className='resultbox'>
+                        <CommandLine />
+                    </div>
+                </div>
+                <div className='row'>
+                    <MyButton name='C' classO='calcbtn'/>
+                    <MyButton name='/'classO='btn_marginal'/>
+                </div>
+                <div className='row'>
+                    <MyButton name='7'/>
+                    <MyButton name='8'/>
+                    <MyButton name='9'/>
+                    <MyButton name='*'classO='btn_marginal'/>
+                </div>
+                <div className='row'>
+                    <MyButton name='4'/>
+                    <MyButton name='5'/>
+                    <MyButton name='6'/>
+                    <MyButton name='-'classO='btn_marginal'/>
+                </div>
+                <div className='row'>
+                    <MyButton name='1'/>
+                    <MyButton name='2'/>
+                    <MyButton name='3'/>
+                    <MyButton name='+' classO='btn_marginal'/>
+                </div>
+                <div className='row'>
+                    <MyButton name='0' classO='calcbtn'/>
+                    <CalcButton classO='btn_marginal'/>
                 </div>
             </div>
-            <div className='row'>
-                <MyButton name='C' classO='calcbtn'/>
-                <MyButton name='/'classO='btn_marginal'/>
-            </div>
-            <div className='row'>
-                <MyButton name='7'/>
-                <MyButton name='8'/>
-                <MyButton name='9'/>
-                <MyButton name='*'classO='btn_marginal'/>
-            </div>
-            <div className='row'>
-                <MyButton name='4'/>
-                <MyButton name='5'/>
-                <MyButton name='6'/>
-                <MyButton name='-'classO='btn_marginal'/>
-            </div>
-            <div className='row'>
-                <MyButton name='1'/>
-                <MyButton name='2'/>
-                <MyButton name='3'/>
-                <MyButton name='+' classO='btn_marginal'/>
-            </div>
-            <div className='row'>
-                <MyButton name='0' classO='calcbtn'/>
-                <CalcButton classO='btn_marginal'/>
-            </div>
         </div>
+       
     </AppProvider>
   )
 }
